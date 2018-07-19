459074e5de1a3f9e401a34ccbcf4f18af464105a
juanraba <jrabanillog@gmail.com>
2018-07-19 13:20:19 -0400

    add ejer1.3

--- ejer1.rb
+++ ejer1.rb
@@ -4,6 +4,14 @@ class Table
     @name = name
     @days = days
   end
+
+  def max
+    @days.each_with_index.max
+  end
+
+  def avg
+    @days.reduce(:+) / @days.size.to_f
+  end
 end
 
 def load_casino(casino)
@@ -11,11 +19,24 @@ def load_casino(casino)
   File.open('casino.txt', 'r') { |file| data = file.readlines }
 
   data.each do |prod|
-    ls = prod.split(', ')
+    ls = prod.chomp.split(', ').map &:to_i # { |e| e.to_i }
     casino << Table.new(*ls)
   end
 end
 
 casino = []
 load_casino(casino)
-puts casino.size
+
+def max_day(casino)
+  casino.each do |table|
+    day = table.max
+    puts "#{day[0]} el dia #{day[1]}"
+  end
+end
+max_day(casino)
+
+def avg(casino)
+  casino.inject (0) { |mem, table| mem + table.avg.to_i }
+end
+puts avg(casino) / casino.size.to_f
+
