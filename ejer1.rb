66b4a2e57044dd0c5d66a65fc86bef91adecc37e
juanraba <jrabanillog@gmail.com>
2018-07-19 12:54:46 -0400

    add ejer1.2

--- ejer1.rb
+++ ejer1.rb
@@ -5,3 +5,17 @@ class Table
     @days = days
   end
 end
+
+def load_casino(casino)
+  data = []
+  File.open('casino.txt', 'r') { |file| data = file.readlines }
+
+  data.each do |prod|
+    ls = prod.split(', ')
+    casino << Table.new(*ls)
+  end
+end
+
+casino = []
+load_casino(casino)
+puts casino.size
