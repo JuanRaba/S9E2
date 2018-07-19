1386c4a780f345ac02fe876a35702d8ddeab0a94
juanraba <jrabanillog@gmail.com>
2018-07-19 13:56:14 -0400

    add ejer2.3

--- ejer2.rb
+++ ejer2.rb
@@ -7,6 +7,20 @@ class Course
     @name = name
     @dates = dates.map { |e| Date.parse(e) }
   end
+
+  def starts_before(date=Date::today)
+    check_date(date)
+    @name if @dates[0] < date
+  end
+
+  def ends_after(date=Date::today)
+    check_date(date)
+    @name if @dates[1] > date
+  end
+
+  def check_date(date)
+    raise ArgumentError, 'date  >= 2018-01-01' if date  >= Date.parse('2018-01-01')
+  end
 end
 
 def load_academi(casino)
@@ -21,3 +35,12 @@ end
 
 academi = []
 load_academi(academi)
+
+date = Date.parse('2017-05-17')
+def show_starts_before(academi, date=Date::today)
+  academi.each do |c|
+    puts c.starts_before(date)
+  end
+end
+show_starts_before(academi, date)
+
